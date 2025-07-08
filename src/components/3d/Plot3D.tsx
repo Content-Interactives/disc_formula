f484ca066deeb42f71134b7cddaf3e933faa4828
MaxMeyberg <mmeyberg@stanford.edu>
2025-07-08 11:34:43 -0700

    added lines for integrals (debugging)

--- src/components/3d/Plot3D.tsx
+++ src/components/3d/Plot3D.tsx
@@ -50,6 +50,14 @@ const Plot3D: React.FC<Plot3DProps> = ({
     upperBound = 0,
     graphSize = 20 // 20 -> 20x20 graph for whole xy grid
 }) => {
+    const safeEval = (func: string, x: number) => {
+        try {
+            return evaluate(func, { x }) as number
+        } catch (e) {
+            return x // fallback
+        }
+    }
+
     return (
         <div style={{ width: "100vw", height: "100vh" }}>
             <Canvas camera={{ position: [4, 4, 10], fov: 60 }}>
@@ -62,8 +70,8 @@ const Plot3D: React.FC<Plot3DProps> = ({
                 
                 {/* NEW: Draw the function curve using the props */}
                 <FunctionCurve func={userFunction} a={lowerBound} b={upperBound} />
-                <Line points={[[lowerBound, evaluate(userFunction, { x: lowerBound }) as number, 0], [lowerBound, 0, 0]]} color="yellow" lineWidth={2} transparent opacity={0.6} />
-                <Line points={[[upperBound, evaluate(userFunction, { x: upperBound }) as number, 0], [upperBound, 0, 0]]} color="yellow" lineWidth={2} transparent opacity={0.6} />
+                <Line points={[[lowerBound, safeEval(userFunction, lowerBound), 0], [lowerBound, 0, 0]]} color="yellow" lineWidth={2} transparent opacity={0.6} />
+                <Line points={[[upperBound, safeEval(userFunction, upperBound), 0], [upperBound, 0, 0]]} color="yellow" lineWidth={2} transparent opacity={0.6} />
 
                 
                 {/* Optional: Grid on the XY plane */}
