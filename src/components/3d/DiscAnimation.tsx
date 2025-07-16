52e71f2a5fd5706f321a81ef74602cb62704b713
MaxMeyberg <mmeyberg@stanford.edu>
2025-07-16 10:06:42 -0700

    made discs

--- src/components/3d/DiscAnimation.tsx
+++ src/components/3d/DiscAnimation.tsx
@@ -1,5 +1,5 @@
 // src/components/3d/DiscAnimation.tsx
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { useFrame } from "@react-three/fiber"
 import { Cylinder } from '@react-three/drei'
 import { evalFn3D } from '../utils/mathUtils'
@@ -26,28 +26,28 @@ const DiscAnimation: React.FC<DiscAnimationProps> = ({
 }) => {
     const [visibleDiscs, setVisibleDiscs] = useState(0)
     
-    // Calculate all disc positions and sizes
+    // Calculate an array of disc positions and sizes
     const discs = React.useMemo(() => {
-        const discData: DiscData[] = []  // specify the type
+        const arr: DiscData[] = []  // specify the type
         
         const dx = 0.1 // thickness of each disc
         
         for (let x = lowerBound; x <= upperBound; x += dx) {
-            const radius = evalFn3D(userFn, x)
-            discData.push({
+
+            arr.push({
                 position: [x, 0, 0] as [number, number, number],
                 rotation: [0, 0, Math.PI/2] as [number, number, number],
-                radius: radius,
+                radius: evalFn3D(userFn, x),
                 height: dx
             })
         }
-        return discData
-    }, [userFn, lowerBound, upperBound])
+        return arr
+    }, [userFn, lowerBound, upperBound]) // This function will run again when the user changes the interactive function
 
-    // Animation frame
+    // Animation frame this runs 60 times per second
     useFrame(() => {
         if (active && visibleDiscs < discs.length) {
-            setVisibleDiscs(prev => prev + 0.1)  // Slow animation
+            setVisibleDiscs(prev => prev + 0.05)  // Slow animation
         } else if (!active) {
             setVisibleDiscs(0)  // Reset when inactive
         }
@@ -58,15 +58,36 @@ const DiscAnimation: React.FC<DiscAnimationProps> = ({
     return (
         <group>
             {discs.slice(0, Math.floor(visibleDiscs)).map((disc, i) => (
-                <Cylinder
-                    key={i}
-                    position={disc.position as [number, number, number]}
-                    rotation={disc.rotation as [number, number, number]}
-                    args={[disc.radius, disc.radius, disc.height, 32]}
-                    material-color="gray"
-                    material-opacity={1}
-                    material-transparent
-                />
+                <>
+                    <Cylinder
+                        key={i}
+                        position={disc.position as [number, number, number]}
+                        rotation={disc.rotation as [number, number, number]}
+                        args={[disc.radius, disc.radius, disc.height, 32]}
+                    >
+                        <meshPhysicalMaterial
+                            color="black"           // Change from gray to black
+                            metalness={0.7}        // Keep some metallic quality
+                            roughness={0.4}        // Vinyl-like surface
+                            opacity={1}            // Make it completely solid
+                            transparent={false}    // Remove transparency
+                        />
+                    </Cylinder>
+
+                    {/* Add a yellow center label */}
+                    <Cylinder
+                        key={`label-${i}`}
+                        position={disc.position as [number, number, number]}
+                        rotation={disc.rotation as [number, number, number]}
+                        args={[disc.radius * 0.5, disc.radius * 0.5, disc.height * 1.01, 32]}
+                    >
+                        <meshStandardMaterial
+                            color="#FFD700"        // Yellow gold color
+                            metalness={0}          // Non-metallic
+                            roughness={0.5}        // Paper-like finish
+                        />
+                    </Cylinder>
+                </>
             ))}
         </group>
     )
